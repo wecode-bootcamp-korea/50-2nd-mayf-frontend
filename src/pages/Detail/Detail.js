5797caf2c47d68b5442033e6e400b9481d9ba52e
shin.heehyun <lupinworld22@gmail.com>
2023-11-08 13:16:33 +0900

    상세페이지 초안

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './Detail.scss';
 
 const Detail = () => {
-  return <div className="Detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="container">
+        <div className="header">
+          <div className="headerTitle">Class Page</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
