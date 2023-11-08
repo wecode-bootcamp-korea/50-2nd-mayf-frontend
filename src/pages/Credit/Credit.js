348ba30f0e9980b0edb94850a02b06f6c71241ed
unknown <alswl8184@naver.com>
2023-11-08 22:23:10 +0900

    Design Credit-UI

--- src/pages/Credit/Credit.js
+++ src/pages/Credit/Credit.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import CreditIcon from './credit.png';
 import './Credit.scss';
 
 const Credit = () => {
-  return <div className="Credit">Credit</div>;
+  return (
+    <div className="credit">
+      <div className="header">
+        <h1 className="title">Credit</h1>
+      </div>
+      <div className="iconBox">
+        <img className="icon" src={CreditIcon} alt="class" />
+      </div>
+      <div className="selectBox">
+        <div className="amount">10,000원</div>
+        <div className="amount">30,000원</div>
+        <div className="amount">50,000원</div>
+      </div>
+      <div>결제위젯(토스)</div>
+      <label className="agreeLabel">
+        <input type="checkbox"></input>
+        약관동의
+      </label>
+
+      <div className="chargeBtnBox">
+        <button type="button" className="btn">
+          <span>충전하기</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Credit;
