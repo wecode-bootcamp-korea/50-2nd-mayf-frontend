baa9e6797f2df7ef3feed1bc0dc39fb93ff9af99
jsm00929@gmail.com <jsm00929@naver.com>
2023-11-08 11:30:44 +0900

    add: login css ver0.1

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './Login.scss';
 
 const Login = () => {
-  return <div className="Login">Login</div>;
+  return (
+    <div className="Login">
+      <div className="container">
+        <div className="user">
+          <button className="loginBtn">하루살이</button>
+        </div>
+        <div className="event">
+          <button className="loginBtn">등대지기</button>
+        </div>
+        <div className="signup">
+          <p>회원가입</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
