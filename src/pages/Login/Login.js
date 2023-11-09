ab4109d638d79b9d89df0daeb4de70b25e5b375d
jsm00929@gmail.com <jsm00929@naver.com>
2023-11-09 15:55:57 +0900

    fix: css ver0.5

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,30 +1,52 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   // 카카오 소셜로그인 인가코드 따오는 부분
   // const params = new URL(document.location.toString()).searchParams;
   // const code = params.get('code');
+  const handleClick = () => {
+    alert('카카오 로그인');
+  };
 
   return (
     <div className="Login">
       <div className="container">
         <div className="user">
           <div className="textbox">for 하루살이</div>
-          <img src="/images/kakao_login_medium_narrow.png" alt="login_user" />
+          <div className="text">
+            <p>평소와는 다른,</p>
+            <p>뭔가 특별한 하루를</p>
+            <p>꿈꾸는 당신</p>
+          </div>
+          <div className="imgbox">
+            <button onClick={handleClick}>
+              <img
+                src="/images/kakao_login_medium_narrow.png"
+                alt="login_user"
+              />
+            </button>
+          </div>
         </div>
+        <div className="hline" />
         <div className="event">
           <div className="textbox">for 등대지기</div>
-          <img src="/images/kakao_login_medium_narrow.png" alt="login_event" />
-        </div>
-        <div className="signup">
-          <span>아직 회원이 아니시라면 </span>
-          <span className="link" onClick={() => navigate('/sign-up')}>
-            회원가입
-          </span>
+          <div className="text">
+            <p>누군가의 새로운</p>
+            <p>하루, 그 앞길에</p>
+            <p>빛을 비춰줄 당신</p>
+          </div>
+          <div className="imgbox">
+            <button onClick={handleClick}>
+              <img
+                src="/images/kakao_login_medium_narrow.png"
+                alt="login_event"
+              />
+            </button>
+          </div>
         </div>
       </div>
     </div>
