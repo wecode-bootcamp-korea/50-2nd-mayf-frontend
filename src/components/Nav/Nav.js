5044ff0b2dd5e4781723a05bf0535c92d74ec3bb
unknown <alswl8184@naver.com>
2023-11-09 20:14:03 +0900

    Nav  UI 일부수정

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import Logo from './logo.png';
-import Heart from './heart.png';
-import User from './user.png';
-import Category from './category.png';
-import Coin from './credit.png';
+import React, { useState } from 'react';
+import Logo from '../Nav/navImg/logo.png';
+import Category from '../Nav/navImg/category.png';
 import './Nav.scss';
 
+//categoryLogo = 전체 카테고리 표시
+//로그인시 하나의 아이콘으로 표시
+//클릭시 useOutsideClick
+
 const Nav = () => {
+  const [login, setLogin] = useState(false);
+
   return (
     <div className="Nav">
       <div className="categoryLogo">
-        <img className="category" src={Category} alt="cetegoryIcon" />
-        <img className="logo" src={Logo} alt="logo" />{' '}
+        <img className="category" src={Category} alt="categoryIcon" />
+        <img className="logo" src={Logo} alt="logo" />
       </div>
-      <div className="content">
-        <div className="header">Home</div>
-        <div className="header">Class</div>
-        <div className="header">Service</div>
-        <div className="header">About</div>
-      </div>
-      <div className="icons">
-        <img className="user icon" src={User} alt="user" />
-        <img className="heart icon" src={Heart} alt="heart" />
-        <img className="coin icon" src={Coin} alt="credit" />
-        <p className="point">보유 포인트 10p</p>
-      </div>
-      <p>로그인 / 회원가입</p>
+
+      <p className="login"> 로그인 </p>
     </div>
   );
 };
 
 export default Nav;
+
+{
+  /* <div className="icons">
+<img className="icon" src={User} alt="user" />
+<img className="icon" src={Heart} alt="heart" />
+<img className="icon" src={Coin} alt="credit" />
+</div> */
+}
