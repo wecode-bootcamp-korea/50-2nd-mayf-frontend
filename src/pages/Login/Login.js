7756f2aa6c8c018d8a28aea4a456ff20c8374780
jsm00929@gmail.com <jsm00929@naver.com>
2023-11-09 19:25:21 +0900

    fix: css ver0.81

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,29 +1,37 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
-  // const navigate = useNavigate();
+  const REST_API_KEY = '90dbff4da803f007ab4c956bfce95fd0';
 
-  // 카카오 소셜로그인 인가코드 따오는 부분
-  // const params = new URL(document.location.toString()).searchParams;
-  // const code = params.get('code');
-  const handleClick = () => {
-    alert('카카오 로그인');
+  const handleClickUser = () => {
+    const REDIRECT_URI = 'http://localhost:3000/users/signin';
+    const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code
+  `;
+    window.location.href = KAKAO_URL;
+  };
+
+  const handleClickEvent = () => {
+    const REDIRECT_URI = 'http://localhost:3000/auth/kakao/callbackevent';
+    const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code
+  `;
+    window.location.href = KAKAO_URL;
   };
 
   return (
     <div className="Login">
       <div className="container">
         <div className="user">
-          <div className="textbox">for 하루살이</div>
+          <div className="textBox">for 하루살이</div>
           <div className="text">
-            <p>평소와는 다른,</p>
-            <p>뭔가 특별한 하루를</p>
-            <p>꿈꾸는 당신</p>
+            평소와는 다른,
+            <br />
+            뭔가 특별한 하루를
+            <br />
+            꿈꾸는 당신
           </div>
-          <div className="imgbox">
-            <button onClick={handleClick}>
+          <div className="imgBox">
+            <button onClick={handleClickUser}>
               <img
                 src="/images/kakao_login_medium_narrow.png"
                 alt="login_user"
@@ -33,14 +41,16 @@ const Login = () => {
         </div>
         <div className="hline" />
         <div className="event">
-          <div className="textbox">for 등대지기</div>
+          <div className="textBox">for 등대지기</div>
           <div className="text">
-            <p>누군가의 새로운</p>
-            <p>하루, 그 앞길에</p>
-            <p>빛을 비춰줄 당신</p>
+            누군가의 새로운
+            <br />
+            하루, 그 앞길에
+            <br />
+            빛을 비춰줄 당신
           </div>
-          <div className="imgbox">
-            <button onClick={handleClick}>
+          <div className="imgBox">
+            <button onClick={handleClickEvent}>
               <img
                 src="/images/kakao_login_medium_narrow.png"
                 alt="login_event"
