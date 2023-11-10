03cba7efba8ce25159954f47717fbe168625642f
unknown <alswl8184@naver.com>
2023-11-10 15:08:03 +0900

    [50기 최민지] Feat : 유저 아이콘 클릭 시 사이드바 생성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Logo from '../Nav/navImg/logo.png';
 import Category from '../Nav/navImg/category.png';
+import UserIcon from '../Nav/navImg/user.png';
 import './Nav.scss';
 
 //categoryLogo = 전체 카테고리 표시
@@ -9,16 +11,42 @@ import './Nav.scss';
 //클릭시 useOutsideClick
 
 const Nav = () => {
-  const [login, setLogin] = useState(false);
+  const navigate = useNavigate();
+  //login여부 확인 state, 나중에는 토큰 불러올 예정
+  const [login, setLogin] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className="Nav">
+    <div className="nav">
       <div className="categoryLogo">
         <img className="category" src={Category} alt="categoryIcon" />
-        <img className="logo" src={Logo} alt="logo" />
+        <img
+          className="logo"
+          onClick={() => {
+            navigate('/');
+          }}
+          src={Logo}
+          alt="logo"
+        />
       </div>
-
-      <p className="login"> 로그인 </p>
+      <div className="loginLogo">
+        {/* <img className="user" src={UserIcon} onClick={toggleSidebar} /> */}
+        {login ? (
+          <div>
+            <img
+              onClick={() => toggleSidebar()}
+              className="user"
+              src={UserIcon}
+            />
+          </div>
+        ) : (
+          <p className="login"> 로그인 </p>
+        )}
+        {isOpen ? <Sidebar /> : null}
+      </div>
     </div>
   );
 };
--- src/components/Nav/Sidebar.js
+++ src/components/Nav/Sidebar.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Sidebar.scss';
 
 const Sidebar = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  return (
+    <div className="sideBar">
+      <ul className="userMenu">
+        <li
+          className="menu"
+          onClick={() => {
+            navigate('/my-page-user');
+          }}
+        >
+          MY PAGE
+        </li>
+        <li className="menu">보유 크레딧</li>
+        <li
+          className="menu"
+          onClick={() => {
+            navigate('/credit');
+          }}
+        >
+          충전하기
+        </li>
+        <li className="menu">WISH LIST</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
