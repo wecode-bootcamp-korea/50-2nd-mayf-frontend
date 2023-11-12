5babb26e59aaff306e57abd16c029f2d0ff8bf5c
unknown <alswl8184@naver.com>
2023-11-12 15:07:04 +0900

    [50기 최민지] Fix : 리뷰 반영

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,32 +6,45 @@ import Logo from '../Nav/navImg/logo.png';
 import Category from '../Nav/navImg/category.png';
 import UserIcon from '../Nav/navImg/user.png';
 import './Nav.scss';
+import { GET_TOP_CATEGORY_LIST_API } from '../../config';
 
 const Nav = () => {
   const navigate = useNavigate();
   //login여부 확인 state, 나중에는 토큰 불러올 예정
   const [login, setLogin] = useState(true);
-  const [isOpen, setIsOpen] = useState(false);
   //마이페이지 사이드바 구현
-  const [categories, setCategories] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+
+  const [categories, setCategories] = useState([]);
+
   //햄버거 버튼 카테고리 구현
   const [showCategories, setShowCategories] = useState(false);
   const handleLogoClick = () => {
     setShowCategories(!showCategories);
   };
-  // 백엔드 통신 데이터
+  // 백엔드 통신 데이터(MOCK DATA)
   useEffect(() => {
     fetch('/data/dummy.json', {
       method: 'GET',
     })
       .then((res) => res.json())
       .then((result) => {
-        setCategories(result.data);
+        setCategories(result.message);
       });
   }, []);
+  // 백엔드 통신 데이터
+  // useEffect(() => {
+  //   fetch(`${GET_TOP_CATEGORY_LIST_API}`, {
+  //     method: 'GET',
+  //   })
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       setCategories(result.message);
+  //     });
+  // }, []);
 
   return (
     <div className="nav">
@@ -53,19 +66,16 @@ const Nav = () => {
         />
       </div>
       <div className="loginLogo">
-        {/* <img className="user" src={UserIcon} onClick={toggleSidebar} /> */}
         {login ? (
-          <div>
-            <img
-              onClick={() => toggleSidebar()}
-              className="user"
-              src={UserIcon}
-            />
-          </div>
+          <img
+            onClick={() => toggleSidebar()}
+            className="user"
+            src={UserIcon}
+          />
         ) : (
           <p className="login"> 로그인 </p>
         )}
-        {isOpen ? <Sidebar /> : null}
+        {isOpen && <Sidebar />}
       </div>
     </div>
   );
--- src/components/Nav/components/CategoryList.js
+++ src/components/Nav/components/CategoryList.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './CategoryList.scss';
 
 const CategoryList = ({ categories }) => {
+  // const uniqueTopCategories = Array.from(
+  //   new Set(classList.message?.map((item) => item.top_category_name)),
+  // );
+
   return (
     <div className="categoryList">
       <ul className="topCategory">
         {categories.map((category) => (
           <li key={category.id} className="categoryMenu">
-            {category.name}
+            {category.top_category_name}
           </li>
         ))}
       </ul>
