677210ba7d5ae6b83033dc588e1d7fcabf6fb654
jsm00929@gmail.com <jsm00929@naver.com>
2023-11-17 18:11:18 +0900

    Feat: 유저 강의 신청내역 관련

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -1,92 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Reservation.scss';
 
 const Reservation = () => {
+  const [reservationList, setReservationList] = useState([
+    {
+      title: '',
+      host_name: '',
+      class_day: '',
+      top_category_name: '',
+      sub_category_name: '',
+      address: '',
+      enrolled_member: '',
+    },
+  ]);
+  // const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getReservationList();
+  }, []);
+
+  const getReservationList = () => {
+    fetch('http://10.58.52.154:8000/classes/myclass', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        // Authorization: token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => setReservationList(data.message));
+  };
+
+  const handleDetail = (itemId) => {
+    navigate(`/detail/${itemId}`);
+  };
+
+  const handleDelete = (itemId) => {
+    fetch(`http://10.58.52.158:8000/orders/${itemId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        // Authorization: token,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert('예약이 성공적으로 취소되었습니다');
+        getReservationList();
+      });
+  };
+
   return (
     <div className="reservation">
       <div className="container">
         <div className="label">예약 내역</div>
 
         <div className="contents">
-          <div className="content">
-            <img
-              src="https://img.freepik.com/free-photo/top-view-arrangement-of-natural-material-stationery_23-2148898233.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1698796800&semt=sph"
-              alt="이미지"
-            />
+          {reservationList.map((item) => {
+            return (
+              <div className="content" key={item.id}>
+                <img src={item.image_source} alt="이미지" />
 
-            <div className="classInfo">
-              <div className="classTitle">강의명 / 강의자</div>
-              <div className="classTime">강의시간</div>
-              <div className="classCategory">강의 카테고리</div>
-              <div className="classLocation">강의 장소</div>
-              <div className="classpeople">1명</div>
-            </div>
+                <div className="classInfo">
+                  <div className="classTitle">강의명 : {item.title}</div>
+                  <div className="hostName">강의자 : {item.host_name}</div>
+                  <div className="classTime">강의시간 : {item.class_day}</div>
+                  <div className="classCategory">
+                    강의 카테고리 : {item.top_category_name}(
+                    {item.sub_category_name})
+                  </div>
+                  <div className="classLocation">
+                    강의 장소 : {item.address}
+                  </div>
+                  <div className="classpeople">
+                    인원 : {item.enrolled_member}
+                  </div>
+                  <div>{item.order_id}</div>
+                </div>
 
-            <div className="buttons">
-              <button>상세 보기</button>
-              <button>예약 취소</button>
-            </div>
-          </div>
-
-          <div className="content">
-            <img
-              src="https://img.freepik.com/free-photo/top-view-arrangement-of-natural-material-stationery_23-2148898233.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1698796800&semt=sph"
-              alt="이미지"
-            />
-
-            <div className="classInfo">
-              <div className="classTitle">강의명 / 강의자</div>
-              <div className="classTime">강의시간</div>
-              <div className="classCategory">강의 카테고리</div>
-              <div className="classLocation">강의 장소</div>
-              <div className="classpeople">1명</div>
-            </div>
-
-            <div className="buttons">
-              <button>상세 보기</button>
-              <button>예약 취소</button>
-            </div>
-          </div>
-
-          <div className="content">
-            <img
-              src="https://img.freepik.com/free-photo/top-view-arrangement-of-natural-material-stationery_23-2148898233.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1698796800&semt=sph"
-              alt="이미지"
-            />
-
-            <div className="classInfo">
-              <div className="classTitle">강의명 / 강의자</div>
-              <div className="classTime">강의시간</div>
-              <div className="classCategory">강의 카테고리</div>
-              <div className="classLocation">강의 장소</div>
-              <div className="classpeople">1명</div>
-            </div>
-
-            <div className="buttons">
-              <button>상세 보기</button>
-              <button>예약 취소</button>
-            </div>
-          </div>
-
-          <div className="content">
-            <img
-              src="https://img.freepik.com/free-photo/top-view-arrangement-of-natural-material-stationery_23-2148898233.jpg?size=626&ext=jpg&ga=GA1.1.1412446893.1698796800&semt=sph"
-              alt="이미지"
-            />
-
-            <div className="classInfo">
-              <div className="classTitle">강의명 / 강의자</div>
-              <div className="classTime">강의시간</div>
-              <div className="classCategory">강의 카테고리</div>
-              <div className="classLocation">강의 장소</div>
-              <div className="classpeople">1명</div>
-            </div>
-
-            <div className="buttons">
-              <button>상세 보기</button>
-              <button>예약 취소</button>
-            </div>
-          </div>
+                <div className="buttons">
+                  <button onClick={() => handleDetail(item.id)}>
+                    상세 보기
+                  </button>
+                  <button onClick={() => handleDelete(item.order_id)}>
+                    예약 취소
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
