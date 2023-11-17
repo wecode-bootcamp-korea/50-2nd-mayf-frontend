ec471dab43f8b16e1f57ce813c8dc645ad1756e3
shin.heehyun <lupinworld22@gmail.com>
2023-11-17 17:42:56 +0900

    Feat : 등대 마이페이지 강의 내역 불러오기 및 삭제 구현

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,27 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import './ClassList.scss';
-import { useNavigate } from 'react-router-dom';
 
 const ClassList = ({ setTab }) => {
   const [classList, setClassList] = useState([]);
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    fetch(`http://10.58.52.181:8000/classes/hostclass`, {
+  const getClassList = () => {
+    fetch(`http://10.58.52.154:8000/classes/hostclass`, {
       headers: {
         Authorization:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzIsIm5hbWUiOiLsnoTsi5ztmIQiLCJlbWFpbCI6ImpzbTAwOTI5QG5hdmVyLmNvbSIsInBob25lX251bWJlciI6Iis4MiAxMC05MTU5LTA1MDYiLCJpYXQiOjE2OTk5MzUyODMsImV4cCI6MTcwMDY1NTI4M30.oXtoBY1SoujOKSClsVQf2JM9QoBuhajNV1EVC3b3R4o',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQsIm5hbWUiOiLstZzrr7zsp4AiLCJlbWFpbCI6ImFsc3dsODE4NEBuYXZlci5jb20iLCJwaG9uZV9udW1iZXIiOiIwMTAtNTcwNC04NDg0Iiwicm9sZSI6Imhvc3RzIiwiaWF0IjoxNzAwMTk2MjQ4LCJleHAiOjE3MDA5MTYyNDh9.djPth_b9BC8H8dNNpr3R0LnuUbC9pQ3oeYlihvzUwyA',
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        setClassList(data);
+        setClassList(data.message);
       });
+  };
+
+  useEffect(() => {
+    getClassList();
   }, []);
 
   // const viewSpecify = () => {
   //   navigate(`/detail/${id}`);
   // };
+
+  const deleteClass = (id) => {
+    fetch(`http://10.58.52.154:8000/classes/delete/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization:
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzQsIm5hbWUiOiLstZzrr7zsp4AiLCJlbWFpbCI6ImFsc3dsODE4NEBuYXZlci5jb20iLCJwaG9uZV9udW1iZXIiOiIwMTAtNTcwNC04NDg0Iiwicm9sZSI6Imhvc3RzIiwiaWF0IjoxNzAwMTk2MjQ4LCJleHAiOjE3MDA5MTYyNDh9.djPth_b9BC8H8dNNpr3R0LnuUbC9pQ3oeYlihvzUwyA',
+      },
+    }).then((res) => {
+      if (res.ok) {
+        getClassList();
+      }
+    });
+  };
 
   return (
     <div className="classList">
@@ -39,20 +56,20 @@ const ClassList = ({ setTab }) => {
         </div>
 
         <div className="contents">
-          {classList.map((data) => (
-            <div className="content">
-              <img src={data.image} alt="이미지" />
+          {classList.map((myClass) => (
+            <div className="content" key={myClass.id}>
+              <img src={myClass.main_image_source} alt="이미지" />
 
               <div className="classInfo">
-                <div className="className">{data.title}</div>
-                <div className="classOpener">{data}</div>
-                <div className="classCategory">{data.category}</div>
-                <div className="classLocation">{data.location}</div>
+                <div className="className">{myClass.title}</div>
+
+                <div className="classCategory">{myClass.top_category_name}</div>
+                <div className="classLocation">{myClass.address}</div>
               </div>
 
               <div className="buttons">
                 <button>상세 보기</button>
-                <button>삭제</button>
+                <button onClick={() => deleteClass(myClass.id)}>삭제</button>
               </div>
             </div>
           ))}
